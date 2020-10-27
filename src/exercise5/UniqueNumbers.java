d946a297eb73ff5c67a88f88fb2603bdc259fe38
AntoninaY01 <a.yordanova01@icloud.com>
2020-10-27 16:30:15 +0000

    2

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -19,7 +19,21 @@ public class UniqueNumbers {
 
         int[] uniqueNumbers = new int[numbers.length];
         // Find unique numbers in numbers
+        boolean found = false;
+        int next = 0;
+        for (int i=0;i<uniqueNumbers.length; i++){
+          for(int j=0; j<numbers.length;j++){
+          if(uniqueNumbers[i]==numbers[i]){
+            found = true;
+            break;
 
+          }
+          }
+           if(!found){
+             uniqueNumbers[next]=numbers[i];
+             next++;
+           }
+        }
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
     }
